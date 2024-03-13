c10a22997b528fc3f67a4fd6344d7abb0f4f6d24
OscarLiang <a7757729@gmail.com>
2024-03-13 21:16:55 +0800

    Add Repo model

---
+++ app/src/main/java/com/oscarliang/gitfinder/model/Repo.kt
@@ -0,0 +1,38 @@
+package com.oscarliang.gitfinder.model
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "repos")
+data class Repo(
+    @PrimaryKey
+    @field:SerializedName("id")
+    val id: Int,
+    @field:SerializedName("name")
+    val name: String,
+    @field:SerializedName("description")
+    val description: String?,
+    @field:SerializedName("owner")
+    @field:Embedded(prefix = "owner_")
+    val owner: Owner,
+    @field:SerializedName("stargazers_count")
+    val stars: Int,
+    @field:SerializedName("forks_count")
+    val forks: Int,
+    @field:SerializedName("language")
+    val language: String,
+    @field:SerializedName("html_url")
+    val url: String,
+    val bookmark: Boolean = false
+) {
+
+    data class Owner(
+        @field:SerializedName("login")
+        val name: String,
+        @field:SerializedName("avatar_url")
+        val avatarUrl: String?
+    )
+
+}
